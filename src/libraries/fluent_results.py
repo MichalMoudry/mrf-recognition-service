cc5785d39122a55bc7a803db76ab9f75dc67c0da
Michal Moudrý <moudry.michal@live.com>
2023-08-23 18:38:10 +0200

    Update fluent_results.py

--- src/libraries/fluent_results.py
+++ src/libraries/fluent_results.py
@@ -1,46 +1,37 @@
 """
 Module with code for fluent results library.
 """
-from typing import TypeVar
+from typing import TypeVar, Generic
 
 T = TypeVar("T")
 
 
-class Result:
+class Result(Generic[T]):
     """
     Class representing a result object that encapsulates a value and an operation result.
     """
     def __init__(self):
-        self._was_result_modified = False
         self._value: T = None
         self._is_success = False
-        self._messages: list[str] = []
-
-    def ok(self, value: T = None):
-        self._was_result_modified = True
-        self._is_success = True
-        self._value = value
-        return self
-
-    def fail(self, msg: str):
-        self._was_result_modified = True
-        self._is_success = False
-        self._messages.append(msg)
-        return self
 
     @property
     def value(self) -> T:
+        """
+        Encapsulated value.
+        """
         return self._value
 
-    @property
-    def messages(self) -> list[str]:
-        return self._messages
+    def ok(self, value: T = None):
+        self._value = value
+        self._is_success = True
 
-    @property
-    def is_success(self) -> bool:
-        return self._is_success
+    def fail(self):
+        """
+        Sets the result object to failed state.
+        """
+        self._is_success = False
 
-    def has_value(self):
+    def has_value(self) -> bool:
         """
         Method for checking if Result object has a value set.
         :return: True if value is set. False if not.

91263ead0833319e829ea359e6c3d5b02c7aeeeb
Michal Moudrý <moudry.michal@live.com>
2023-08-24 20:51:00 +0200

    Updated fluent results

--- src/libraries/fluent_results.py
+++ src/libraries/fluent_results.py
@@ -2,8 +2,40 @@
 Module with code for fluent results library.
 """
 from typing import TypeVar, Generic
+from enum import Enum
 
 T = TypeVar("T")
+
+
+class ReasonType(Enum):
+    """
+    Enumeration representing possible types of a reason for a result.
+    """
+    ERROR = 0
+    SUCCESS = 1
+
+
+class Reason:
+    """
+    Class representing a reason (success or failure) for the result.
+    """
+    def __init__(self, reason_type: ReasonType, reason: str):
+        self._reason_type: ReasonType = reason_type
+        self._reason: str = reason
+
+    @property
+    def reason_type(self) -> ReasonType:
+        return self._reason_type
+
+
+class Success(Reason):
+    def __init__(self, reason: str):
+        super().__init__(ReasonType.SUCCESS, reason)
+
+
+class Fail(Reason):
+    def __init__(self, reason: str):
+        super().__init__(ReasonType.ERROR, reason)
 
 
 class Result(Generic[T]):
@@ -11,8 +43,9 @@ class Result(Generic[T]):
     Class representing a result object that encapsulates a value and an operation result.
     """
     def __init__(self, result: bool, value: T = None):
-        self._result = result
-        self._value = value
+        self._result: bool = result
+        self._value: T | None = value
+        self._reasons: list[Reason] = []
 
     @property
     def value(self) -> T | None:

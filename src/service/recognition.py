1bec58c54d8496b02ca3509e34cfb3b5f9a21a2b
Michal Moudrý <moudry.michal@live.com>
2023-09-07 13:06:46 +0200

    Recognition service refactoring

--- src/service/recognition.py
+++ src/service/recognition.py
@@ -1,39 +1,51 @@
 """
 Module for a service providing recognition functionality in this service.
 """
+from abc import ABC, abstractmethod
 from PIL import Image
 from os import environ
 import pytesseract
 
 
-class RecognitionService:
+class RecognitionService(ABC):
     """
-    A class representing the entire public functionality of the recognition module.
+    An abstract class representing a generic service for recognizing documents.
     """
-    def __init__(self, tess_path: str | None = None):
+    default_language = "eng"
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    @property
+    def available_languages(self) -> list[str]:
+        """
+        A list of supported languages by the recognition service/system.
+        """
+        ...
+
+    @abstractmethod
+    def process_img_by_path(self, path: str, lang: str | None = None) -> str:
+        """
+        Processes an image based on a specified path.
+        """
+        ...
+
+
+class TesseractService(RecognitionService):
+    """
+    An implementation of a recognition service that is based in Tesseract.
+    """
+    def __init__(self, tess_path: str | None = None) -> None:
+        super().__init__()
         if tess_path == None:
             self._tesseract_path = environ["TESSERACT_PATH"]
         else:
             self._tesseract_path = tess_path
-        self._default_lang = "eng"
-    
-    @property
-    def tesseract_path(self) -> str:
-        """
-        A path to the Tesseract executable file.
-        """
-        return self._tesseract_path
-    
+
     @property
     def available_languages(self):
-        """
-        A list of supported languages by Tesseract.
-        """
         return pytesseract.get_languages(config="")
-
-    def process_image_by_path(self, path: str, lang: str | None = None) -> str:
-        """
-        Processes an image based on a specified path.
-        """
-        language = lang if lang is not None else self._default_lang
+    
+    def process_img_by_path(self, path: str, lang: str | None = None) -> str:
+        language = lang if lang is not None else super().default_language
         return pytesseract.image_to_string(Image.open(path), language)
--- test/test_recognition.py
+++ test/test_recognition.py
@@ -2,26 +2,28 @@
 Module for testing recognition service.
 """
 from os import environ, path, getcwd
-from src.service.recognition import RecognitionService
+from pytest import mark
+from src.service.recognition import RecognitionService, TesseractService
 
 
 def test_service_setup():
     """
     A test where setup of the recognition service is tested.
     """
-    path = "/opt/homebrew/bin/tesseract"
-    environ["TESSERACT_PATH"] = path
-    service = RecognitionService()
+    environ["TESSERACT_PATH"] = "/opt/homebrew/bin/tesseract"
+    service = TesseractService()
     assert service.available_languages == ["eng", "osd", "snum"]
-    assert service.tesseract_path == path
+    assert isinstance(service, RecognitionService) is True
+    assert issubclass(TesseractService, RecognitionService) is True
 
 
+@mark.skip(reason="Can be allowed to run on systems with recognition capabilities")
 def test_img_recognition_by_path():
     """
     A test where a test image specified by path is recognized.
     """
-    service = RecognitionService("/opt/homebrew/bin/tesseract")
-    result = service.process_image_by_path(
+    service = TesseractService("/opt/homebrew/bin/tesseract")
+    result = service.process_img_by_path(
         path.join(getcwd(), "test/test_images/repo_screenshot.png")
     )
     assert "Identity microservice example" in result

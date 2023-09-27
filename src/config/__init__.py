640cfde26e9257ee126bd3540367e2a673c9858d
Michal Moudrý <moudry.michal@live.com>
2023-09-27 12:59:35 +0200

    Added config module

---
+++ src/config/__init__.py
@@ -0,0 +1,17 @@
+"""
+Module for handling recognition service's configuration.
+"""
+from os import environ
+from dotenv import load_dotenv
+
+CONFIG: dict[str, str] = {}
+
+
+def load_configuration():
+    """
+    Function for loading service's configuration.
+    """
+    load_dotenv()
+    db_conn = environ.get("DB_CONN")
+    if db_conn != None:
+        CONFIG["DB_CONN"] = db_conn
---
+++ src/tests/test_config.py
@@ -0,0 +1,16 @@
+"""
+Package with tests related to service's configuration.
+"""
+from src.config import load_configuration, CONFIG
+
+
+def test_basic_cfg_load():
+    """
+    A test scenario for testing environment values before and after config load.
+    """
+    before = CONFIG.get("DB_CONN")
+    assert before is None
+
+    load_configuration()
+    db_conn = CONFIG["DB_CONN"]
+    assert db_conn is not None and len(db_conn) > 0
